df28d76d35699b786cfb1f8aec73e21cabba0889
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-04-25 14:46:08 +0700

    Add Title

--- client/src/pages/PageNotFound.js
+++ client/src/pages/PageNotFound.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/PageNotFoundStyles.css';
+import Meta from '../components/Meta';
 
 const PageNotFound = () => {
     return (
-        <div className="pnf">
-            <h1 className="pnf-title">404</h1>
-            <h2 className="pnf-heading">Oop ! Page Not Found</h2>
-            <Link to="/" className="pnf-btn">
-                Go Back
-            </Link>
-        </div>
+        <>
+            <Meta title={'Page Not Found'} />
+            <div className="pnf">
+                <h1 className="pnf-title">404</h1>
+                <h2 className="pnf-heading">Oop ! Page Not Found</h2>
+                <Link to="/" className="pnf-btn">
+                    Go Back
+                </Link>
+            </div>
+        </>
     );
 };
 
